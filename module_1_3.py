3050bbbae737b86e1aab2987a7542175b58ad875
Chemagini <chemagini@mail.ru>
2024-08-05 08:51:02 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = ("Igor")
 print('Name:',name,type(name))
 age = 30
 print('Age:',age,type(age))
-age = 32
+age = age + 2
 print('New age:',age,type(age))
 is_student = "True"
 print('is Student:',is_student,type(is_student))
