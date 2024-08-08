fd88cb8120ffd49b707053618af08336e0f1c51c
Chemagini <chemagini@mail.ru>
2024-08-08 12:45:39 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,8 @@
-immutable_var = 1, 'привет'
-print(immutable_var)
+# immutable_var = 1, 'привет'
+# print(immutable_var)
 # immutable_var [1] = 100
 # print(immutable_var)
-mutable_list = ([30, 10,], 5)
-print(mutable_list)
-mutable_list [0][0]= 2
+mutable_list = [30, 10, 5]
+print(mutable_list,type(mutable_list))
+mutable_list [0]= 2
 print(mutable_list)
