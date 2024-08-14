d11f33c9a75564293130ba5e25150682a2c4fe1b
Chemagini <chemagini@mail.ru>
2024-08-14 10:46:21 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,28 @@
+# first = 1
+# second = 2
+# third = 3
+# if first == second == third :
+#     print('3 одинаковых числа')
+# elif (first == second) or (first == third) or (second == third):
+#   print("2 одинаковых числа")
+# else:
+#   print("0 одинаковых чисел")
+# first = 1
+# second = 1
+# third = 5
+# if first == second == third :
+#     print('3 одинаковых числа')
+# elif (first == second) or (second == third):
+#   print("2 одинаковых числа")
+# else:
+#   print("0 одинаковых чисел")
+# first = 1
+# second = 1
+# third = 1
+# if first == second == third :
+#     print('3 одинаковых числа')
+# elif (first == second) or (second == third):
+#   print("2 одинаковых числа")
+# else:
+#   print("0 одинаковых чисел")
+
