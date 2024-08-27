61dcfe945aa048e73ec6a9bb3ab714e181f8fae9
Chemagini <chemagini@mail.ru>
2024-08-27 16:23:40 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,10 @@
+calls = 0
+def count_calls():
+    calls = 2
+    string_info = 'mike mouse', 'Hello'
+    is_contains = 'Welcome', 2
+    print(calls)
+    print(string_info, is_contains)
+print(calls)
+count_calls()
+print(calls)
